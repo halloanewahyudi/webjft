2be54745b103f884c3810643013430de195fc375
halloanewahyudi <hallo.anewahyudi@gmail.con>
2025-07-15 17:31:44 +0700

    site-title

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,7 +2,8 @@
   <div class="py-3 bg-dark  fixed w-full z-50 top-0 left-0 lg:absolute lg:bg-transparent">
     <div class="container">
     <nav class="flex justify-between gap-4">
-      <h1 class="font-bold text-2xl font-jft">JFT</h1>
+    <nuxt-link to="/" class="text-2xl font-jft ">JFT</nuxt-link>
+     
       <button @click="openMenu = !openMenu" class="text-3xl block lg:hidden"> <Icon name="fluent:list-rtl-20-filled" /> </button>
      
       <div :class="openMenu ? 'block' : 'hidden lg:block'" class="menu absolute top-[60px] lg:top-auto left-0 w-full p-4 lg:p-0 bg-dark lg:bg-transparent lg:static lg:w-max">
