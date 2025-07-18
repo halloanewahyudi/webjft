7132b9d4c83d1e9510ff2d5b660087bf0ca013af
halloanewahyudi <hallo.anewahyudi@gmail.con>
2025-07-18 09:50:18 +0700

    open menu false

--- app.vue
+++ app.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
-  <Transition name="slide-up" >
-    <div
-      v-if="progress"
-      ref="loaderRef"
-      class="load-layer fixed top-0 left-0 w-full h-1  bg-light z-50 flex flex-col justify-center items-center duration-300 " :style="{ height: `${progress}%` }"
-    >
-      <span class="text-3xl font-jft text-load animate-bounce text-primary">JFT</span>
 
-    </div>
-</Transition>
     <NuxtLayout>
       <nuxt-page />
     </NuxtLayout>
@@ -18,23 +9,5 @@
 
 <script setup lang="ts">
 
-import gsap from 'gsap'
-
- const { progress, isLoading, start, finish, clear } = useLoadingIndicator({
-    duration: 3000,
-    throttle: 200,
-    // This is how progress is calculated by default
-    estimatedProgress: (duration, elapsed) => (2 / Math.PI * 100) * Math.atan(elapsed / duration * 100 / 50)
-  })
-
-  onMounted(() => {
-    gsap.to('.load-layer', {
-      height: 0,
-      duration: 1,
-      onComplete: () => {
-        isLoading.value = false
-      }
-    })  
-  })
 
 </script>
--- components/navbar.vue
+++ components/navbar.vue
@@ -1,4 +1,15 @@
 <template>
+   <Transition name="slide-up" >
+    <div
+      v-if="progress"
+      ref="loaderRef"
+      class="load-layer fixed top-0 left-0 w-full h-1  bg-light z-50 flex flex-col justify-center items-center duration-300 " :style="{ height: `${progress}%` }"
+    >
+      <span class="text-3xl font-jft text-load animate-bounce text-primary">JFT</span>
+
+    </div>
+</Transition>
+
   <div :class="['py-3 fixed w-full z-50 transition-all duration-300', isScrolled ? 'bg-white shadow-md' : 'bg-transparent']">
     <div class="container">
       <nav class="flex justify-between items-center gap-4 transition-colors duration-300 py-2" :class="isScrolled ? 'text-dark' : 'text-light'">
@@ -54,10 +65,28 @@ const { menus } = useMenus();
 const scrollY = ref(0);
 const isScrolled = computed(() => scrollY.value > 50);
 
+
+ const { progress, isLoading, start, finish, clear } = useLoadingIndicator({
+    duration: 3000,
+    throttle: 200,
+    // This is how progress is calculated by default
+    estimatedProgress: (duration, elapsed) => (2 / Math.PI * 100) * Math.atan(elapsed / duration * 100 / 50)
+  })
+
+
 onMounted(() => {
   window.addEventListener('scroll', () => {
     scrollY.value = window.scrollY;
   });
+
+  
+
+});
+
+watchEffect(() => {
+  if(progress.value == 100) {
+   openMenu.value = false
+  }
 });
 
 </script>
