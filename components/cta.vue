b85f1946a85d3bc1434f46ace8820da0e758059d
halloanewahyudi <hallo.anewahyudi@gmail.con>
2025-07-19 13:32:53 +0700

    cta

--- components/cta.vue
+++ components/cta.vue
@@ -2,7 +2,7 @@
   <div class="p-6 border border-gray-300">
      <div class="flex justify-between items-center gap-4 flex-wrap">
        <h2 class="text-3xl  font-bold ">
-          We Are Top Lawyers With 30 Years Os Experience
+          We Are Top Lawyers With 50 Years Of Experience
         </h2>
         <nuxt-link to="#" class="btn">Contact Us</nuxt-link>
      </div>
